8fe193e46bd428f1acb497c602dfc0ddc397e6df
teerawat7 <teerawat@igeargeek.com>
2022-05-18 21:07:32 +0700

    add magin

---
+++ .nuxt/composition-api/globals.mjs
@@ -0,0 +1,5 @@
+export const isFullStatic = false
+export const staticPath = "C:/Users/milez/OneDrive/เดสก์ท็อป/timesheetv2/report-success/.nuxt/static-json"
+export const publicPath = "/"
+export const globalContext = "__NUXT__"
+export const globalNuxt = "$nuxt"
--- components/Popup.vue
+++ components/Popup.vue
@@ -2,10 +2,10 @@
 
   <div class="popup">
     <div class="popup-inner" >
-      <v-card class="border-4 grid justify-items-center" color="#fcfcfc">
-        <img id="imgcheck" src="@/static/check.png" height="250" width="250" />
+      <v-card id="pop2" class="border-4 grid justify-items-center" color="#fcfcfc">
+        <img id="imgcheck" src="@/static/check.png" />
         <v-card-title class="md:text-xl text-sm font-bold text-black">
-		  <slot />
+            <div class="text-s"><slot /></div>
         </v-card-title>
       </v-card>
     </div>
@@ -25,6 +25,7 @@ props: {
 </script>
 
 <style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Itim&display=swap');
 .popup {
   position: fixed;
   top: 0;
@@ -33,22 +34,30 @@ props: {
   bottom: 0;
   z-index: 99;
   background-color: rgba(0, 0, 0, 0.2);
-
   display: flex;
   align-items: center;
   justify-content: center;
 
   .popup-inner {
-    background: #fff;
     padding: 32px;
   }
+.text-s{
+  font-family: 'Itim', cursive;
+  margin-top: 30px;
+  margin-bottom: -50px;
+}
+#pop2{
+  background:black;
+  border-radius: 25px;
+  padding: 80px;
+}
 #imgcheck{
-	width: 120px;
-    height: 100px;
-    display: flex;
-  align-items: center;
-  justify-content: center;
-  background: black;
+	width: 150px;
+  display: flex;
+  // align-items: center;
+  // justify-content: center;
+  margin: auto;
+  margin-top: -50px;
 }
 }
 </style>
